b5fa4c4206023cd643cb24af135bb1972ef71019
sroy96 <sauravroy.eee@gmail.com>
2019-11-03 15:22:37 +0530

    Final Commit

--- Spring_Boot_SQL_connect/src/main/java/Demo/SQL/SQLDemo/Services/ValidityPipelineService.java
+++ Spring_Boot_SQL_connect/src/main/java/Demo/SQL/SQLDemo/Services/ValidityPipelineService.java
@@ -9,9 +9,9 @@ import java.util.Date;
 
 @Service
 public class ValidityPipelineService {
-  boolean result=true;
-    public boolean decision(String status, String enddate, String extendedDate) throws ParseException {
 
+    public boolean decision(String status, String enddate, String extendedDate) throws ParseException {
+        boolean result = false;
         if(status.equals("Approved")){
             result=true;
         }
@@ -22,16 +22,18 @@ public class ValidityPipelineService {
             System.out.print("Pending");
             SimpleDateFormat formatter = new SimpleDateFormat("mm/dd/yyyy");
             Date today= new Date();
+            String parsetoday=formatter.format(today);
+            today=formatter.parse(parsetoday);
             try{
                 Date EndDate= formatter.parse(enddate);
                 System.out.println("len"+extendedDate.length());
-                if(today.compareTo(EndDate)>0){
-                    result=true;
+                if(today.compareTo(EndDate) >= 0 && extendedDate.length()==0){
+                    result=false;
                 }
                 else if(extendedDate.length()>0){
                     Date Extended_Date= formatter.parse(extendedDate);
                     System.out.print("Extended Date:"+Extended_Date);
-                    if(today.compareTo(Extended_Date)>0){
+                    if(today.compareTo(Extended_Date)>=0){
                         result=false;
                     }
                     else{
@@ -43,6 +45,7 @@ public class ValidityPipelineService {
                 }
             }
             catch(ParseException e){
+
                 throw new RuntimeException("Parsing Exception=>"+e.getMessage());
             }
 
